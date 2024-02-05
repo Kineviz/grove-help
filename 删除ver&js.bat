96891996657c28695c727ed8651d76e3b38bf471
unknown <xing@kineviz.com>
2024-02-05 23:37:50 +0800

    test

--- 删除ver&js.bat
+++ 删除ver&js.bat
@@ -2,4 +2,5 @@ REM @echo off
 REM This short script will only remove dlls from my project... or will it?
 del /S *.js
 REM find . -type d -name '.ver' -print -exec rm -rf {} \;
+for /r ./ %%i in (*.grove) do ren "%%i" "%%~ni"
 pause
