de3c8925c4429b27f5b2376f5a4535d282bf6b09
xing <xing@kineviz.com>
2025-11-15 21:24:44 +0800

    threejs

--- 删除ver&js.bat
+++ 删除ver&js.bat
@@ -1,6 +1,6 @@
 REM @echo off
 REM This short script will only remove dlls from my project... or will it?
-del /S *.js
+REM del /S *.js
 REM find . -type d -name '.ver' -print -exec rm -rf {} \;
 for /r ./ %%i in (*.grove) do ren "%%i" "%%~ni"
 REM for /r ./ %%i in (*.ver) do echo ren "%%i" "%%~ni"
